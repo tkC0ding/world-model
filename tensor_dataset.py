d00b8a1da0af4bebecbf1c0e5018901d7c6ebd2e
tkC0ding <tkjagani@gmail.com>
2026-03-31 12:55:27 -0700

    fixed critical data issue

--- tensor_dataset.py
+++ tensor_dataset.py
@@ -4,6 +4,7 @@ import json
 import cv2
 import numpy as np
 import pickle
+import torch
 
 class CarRacingDataset(Dataset):
     def __init__(self, data_dir="", transforms=None):
@@ -30,6 +31,7 @@ class CarRacingDataset(Dataset):
                     img = img.astype(np.float32) / 255.0
                     img = np.transpose(img, (2, 0, 1))
                     action = data[idx % 1000]["action"]
+                    action = torch.tensor(action, dtype=torch.float32)
                     if self.transforms:
                         img = self.transforms(img)
                     return img, action
@@ -43,10 +45,16 @@ class RNN_Dataset(Dataset):
         for episode in self.data:
             for i in range(0, len(episode)-17, 16):
                 self.x.append(episode[i:i+32])
-                self.y.append([i[0] for i in episode[i+1:i+33]])
+                self.y.append([step[0] for step in episode[i+1:i+33]])
     
     def __len__(self):
         return len(self.x)
     
     def __getitem__(self, idx):
-        return self.x[idx], self.y[idx]
+        x_seq = self.x[idx]
+        y_seq = self.y[idx]
+
+        x_tensor = torch.stack([torch.cat([z, action]) for z, action in x_seq])
+        y_tensor = torch.stack(y_seq)
+        
+        return x_tensor, y_tensor
